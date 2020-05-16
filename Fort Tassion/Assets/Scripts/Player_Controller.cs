198a5a7e2976392144fd30e01e392dae25e626a3
Surya Jasper <suryajasper2004@gmail.com>
2020-05-15 23:00:33 -0700

    Created basic player controller

---
+++ Fort Tassion/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Controller : MonoBehaviour
+{
+    private Animator animator;
+    private CharacterController controller;
+    private Vector3 velocity;
+
+    public Camera cam;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float distanceToGround;
+    public LayerMask groundMask;
+
+    [Header("Movement")]
+    public float jumpForce = 3f;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        bool isGrounded = Physics.CheckSphere(groundCheck.position, distanceToGround, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        animator.SetFloat("speed", Mathf.Abs(z));
+
+        Vector3 move = (transform.right * x + transform.forward * z) * speed;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
+        }
+
+        controller.Move(move * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Fort Tassion/Assets/Scripts/cameraScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+    public float sensitivity;
+
+    public Transform player;
+
+    private float xRot = 0.0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        player.Rotate(Vector3.up * mouseX);   
+    }
+}
